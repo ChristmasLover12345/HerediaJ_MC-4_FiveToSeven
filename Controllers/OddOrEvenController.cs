a1b93672062032915b8a8030de31b27153b165e4
ChristmasLover12345 <madnes0205@gmail.com>
2025-01-08 15:04:31 -0800

    Added Odd Or Even

--- Controllers/OddOrEvenController.cs
+++ Controllers/OddOrEvenController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HerediaJ_MC_4_FiveToSeven.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HerediaJ_MC_4_FiveToSeven.Controllers
@@ -11,5 +12,21 @@ namespace HerediaJ_MC_4_FiveToSeven.Controllers
     public class OddOrEvenController : ControllerBase
     {
         
+        private readonly OddOrEvenService _oddOrEvenService;
+
+        public OddOrEvenController(OddOrEvenService oddOrEvenService)
+        {
+            _oddOrEvenService = oddOrEvenService;
+        }
+
+        [HttpGet]
+        [Route("/{number}")]
+
+        public string OddOrEven(string number)
+        {
+            return _oddOrEvenService.OddOrEven(number);
+        }
+
+
     }
 }
--- Services/OddOrEvenService.cs
+++ Services/OddOrEvenService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace HerediaJ_MC_4_FiveToSeven.Services
@@ -8,5 +9,30 @@ namespace HerediaJ_MC_4_FiveToSeven.Services
     public class OddOrEvenService
     {
         
+    public string OddOrEven(string number)
+    {
+
+        bool validation = int.TryParse(number, out int convertedNum);
+
+        if (validation == true)
+        {
+
+            if (convertedNum % 2 == 0)
+            {
+                return $"{number} is Even";
+            }
+            else
+            {
+                return $"{number} is Odd";
+            }
+
+        }
+        else
+        {
+            return "Numbers only";
+        }
+
+    }
+
     }
 }
